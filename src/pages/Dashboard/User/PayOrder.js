a56f209c444ccdb9b5d501bf30af3a3fc8fbc000
coderyeasin <yeasin3233@gmail.com>
2022-04-18 23:06:03 +0600

    test

--- src/pages/Dashboard/User/PayOrder.js
+++ src/pages/Dashboard/User/PayOrder.js
@@ -11,10 +11,10 @@ const PayOrder = () => {
             
         <form onSubmit={handleSubmit(onSubmit)}>
           <input {...register("firstName")} />
-          <select {...register("gender")}>
-            <option value="female">female</option>
-            <option value="male">male</option>
-            <option value="other">other</option>
+          <select {...register("money")}>
+            <option value="yes">$sign</option>
+            <option value="no">male</option>
+            <option value="other">Test</option>
           </select>
           <input type="submit" />
         </form>
