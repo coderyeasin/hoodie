9be17a92915876f8d97ef54f11850c4f8f05507f
coderyeasin <yeasin3233@gmail.com>
2022-04-18 23:05:04 +0600

    add .

--- src/pages/Dashboard/User/PayOrder.js
+++ src/pages/Dashboard/User/PayOrder.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
+
 
 const PayOrder = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = (data) => console.log(data);
     return (
-        <div>
+      <div>
             <h3>Payment Order : Stripe Implement</h3>
-        </div>
+            
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input {...register("firstName")} />
+          <select {...register("gender")}>
+            <option value="female">female</option>
+            <option value="male">male</option>
+            <option value="other">other</option>
+          </select>
+          <input type="submit" />
+        </form>
+      </div>
     );
 };
 
