39e829cda5d80c8ed657c1030da7e312060ace61
coderyeasin <yeasin3233@gmail.com>
2022-04-20 23:51:42 +0600

    chk

--- src/pages/Dashboard/User/PayOrder.js
+++ src/pages/Dashboard/User/PayOrder.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useForm } from "react-hook-form";
 import { useParams } from 'react-router-dom';
 
 
 const PayOrder = () => {
-    const{id} = useParams()
+    const { id } = useParams()
+    
+
+
     const { register, handleSubmit } = useForm();
     const onSubmit = (data) => console.log(data);
     return (
