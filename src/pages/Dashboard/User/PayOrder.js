aa61f2abb7ddd751b7f2999fbe2d39ebcce55787
coderyeasin <yeasin3233@gmail.com>
2022-04-21 22:52:10 +0600

    add price

--- src/pages/Dashboard/User/PayOrder.js
+++ src/pages/Dashboard/User/PayOrder.js
@@ -1,27 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useForm } from "react-hook-form";
 import { useParams } from 'react-router-dom';
 
 
 const PayOrder = () => {
     const { id } = useParams()
-    
-// useEffect(() => {
-//   const url = `https://warm-falls-65459.herokuapp.com/orders?email=${user?.email}`;
-//   fetch(url)
-//     .then((res) => res.json())
-//     .then((data) => {
-//       const filter = data.filter((e) => e.serviceName);
-//       setBooked(filter);
-//     });
-// }, []);
+    const [order, setOrder] = useState({})
+    useEffect(() => {
+        fetch(`http://localhost:5000/orders/${id}`).then(res => res.json()).then(data => setOrder(data))
+    },[order])
 
-    const { register, handleSubmit } = useForm();
-    const onSubmit = (data) => console.log(data);
     return (
       <div>
             <h3>Payment Order : Stripe Implement {id}</h3>
-            
+            <h4>Pay this: ${order?.price}</h4>
        
       </div>
     );
