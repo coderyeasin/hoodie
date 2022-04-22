c28361f26257d559e563e5fa2aceb37de19d4553
coderyeasin <yeasin3233@gmail.com>
2022-04-22 22:54:43 +0600

    credit_form

--- src/pages/Dashboard/User/CheckoutForm.js
+++ src/pages/Dashboard/User/CheckoutForm.js
@@ -1,12 +1,39 @@
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
 import React from 'react';
 
 
-const CheckoutForm = ({order}) => {
-    return (
-        <div>
-            <h3>Checkout {order.price}</h3>
-        </div>
-    );
+const CheckoutForm = ({ hoodie }) => {
+    const stripe = useStripe()
+    const element = useElements()
+    
+    const handleSubmit = e => {
+        e.preventDefault()
+    }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
+              },
+            },
+          }}
+        />
+        <button type="submit" disabled={!stripe}>
+          Pay
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default CheckoutForm;
